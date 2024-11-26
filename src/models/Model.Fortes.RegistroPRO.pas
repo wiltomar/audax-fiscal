2fd39551475bb52575a55f959296d5e81634019f
HelderFernandes1717 <helder.oliveira.fernandes@gmail.com>
2024-11-26 14:09:56 -0300

    Atualizacao dos registros CAB, PAR, GRP, UND, NOP para o FORTES

--- src/models/Model.Fortes.RegistroPRO.pas
+++ src/models/Model.Fortes.RegistroPRO.pas
@@ -6,6 +6,20 @@ uses
   System.Classes, Fortes.IRegistro, Model.Fortes.RegistroOUM, Generics.Collections;
 
 type
+  TTipoUnidadeMedidaDIEF = (
+    tsLitro = 1, tsMetro = 2, tsMetroCubico = 3, tsMetroQuadrado = 4, tsPar = 5,
+    tsQuilograma = 6, tsQuilowattHora = 7, tsUnidade = 8
+  );
+
+type
+  TUnidadeMedidaCENFOP = (
+    tsArroba = 1, tsBarril = 2, tsBalde = 3, tsCento = 4, tsCartela = 5, tsCaixa = 6,
+    tsCaixote = 7, tsDuzia = 8, tsEmbalagem = 9, tsEnvelope = 10, tsFardo = 11,
+    tsGarrafa = 12, tsGalao = 13, tsQuilogramaCENFOP = 14, tsQuloLitro = 15, tsQuiloWattHoraCENFOP = 16
+    );
+
+
+type
   TRegistroPRO = class(TInterfacedObject, IRegistro)
   private
     FTipoRegistro: string;
@@ -15,6 +29,8 @@ type
     FCodigoNCM: string;
     FUnidadeMedida: string;
     FOutraUnidade: TList<TRegistroOUM>;
+    FUnidadeMedidaDIEF: TtipoUnidadeMedidaDIEF;
+    FUnidadeMedidaCENFOP: TUnidadeMedidaCENFOP;
   public
     constructor Create;
     destructor Destroy;
@@ -25,9 +41,21 @@ type
     property Descricao: string read FDescricao write FDescricao;
     property CodigoUtilizadoEstab: string read FCodigoUtilizadoEstab write FCodigoUtilizadoEstab;
     property CodigoNCM: string read FCodigoNCM write FCodigoNCM;
+
     property UnidadeMedida: string read FUnidadeMedida write FUnidadeMedida;
-  //  property OutraUnidade: TList<TRegistroOUM> read FOutraUnidade write FOutraUnidade;
+    property OutraUnidade: TList<TRegistroOUM> read FOutraUnidade write FOutraUnidade; //VER
+    property UnidadeMedidaDIEF: TTipoUnidadeMedidaDIEF read FUnidadeMedidaDIEF write FUnidadeMedidaDIEF;
+    property UnidadeMedidaCENFOP: TUnidadeMedidaCENFOP read FUnidadeMedidaCENFOP write FUnidadeMedidaCENFOP;
+
   end;
+
+
+
+//  TTipoSituacao = (
+//    tsNormal = 0, tsCancelado = 1, tsNormalExtemporaneo = 2, tsCanceladoExtemporaneo = 3,
+//     = 4, tsNFeInutilizada = 5, tsDFComplementar = 6, tsDFRegimeEspecial = 8);
+//
+//  TTipoGNRE = (tgSubstituicao = 1, tgImportacao = 2, tgDifal = 3);
 
 implementation
 
@@ -55,13 +83,16 @@ function TRegistroPRO.GerarLinha: string;
 var
   Linha: TStringList;
 begin
-  var Produto := Format('%s|%s|%s|%s|%s|%s', [
+  var Produto := Format('%s|%s|%s|%s|%s|%s|%s|%s|%s', [
     FTipoRegistro,
     FCodigo,
     FDescricao,
     FCodigoUtilizadoEstab,
     FCodigoNCM,
-    FUnidadeMedida
+    FUnidadeMedida,
+    FOutraUnidade,
+    IntTostr(Integer(FUnidadeMedidaDIEF)),
+    IntTostr(Integer(FUnidadeMedidaCENFOP))
   ]);
 
   Linha := TStringList.Create;
